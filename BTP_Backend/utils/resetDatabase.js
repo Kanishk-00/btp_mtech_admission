2dee7d35721549e12b74356591b34b380f622e4d
Vaibhav <vaibhavtomar.vs@gmail.com>
2024-03-17 06:14:01 +0530

    Added the code to delete round files if available when clicking the reset button on initialise page

--- BTP_Backend/utils/resetDatabase.js
+++ BTP_Backend/utils/resetDatabase.js
@@ -8,7 +8,7 @@ async function resetDatabase(branch) {
   const branchFolder = path.join(userFilePath, branch);
   const modifiedFilePath = path.join(branchFolder, "modifiedFile.xlsx");
   const uploadedFilePath = path.join(branchFolder, "uploadedFile.xlsx");
-
+  
   // Deleting files
   if (fs.existsSync(modifiedFilePath)) {
     fs.unlinkSync(modifiedFilePath);
@@ -17,6 +17,27 @@ async function resetDatabase(branch) {
   if (fs.existsSync(uploadedFilePath)) {
     fs.unlinkSync(uploadedFilePath);
     console.log(`${uploadedFilePath} was deleted`);
+  }
+
+  // Also Delete Round Details if available.
+
+  const generatedOffersPath = path.join(branchFolder, "generatedOffers");
+  const roundUpdatedPath = path.join(branchFolder, "roundUpdates");
+
+  if (fs.existsSync(generatedOffersPath)) {
+    fs.readdirSync(generatedOffersPath).forEach(file => {
+      const filePath = path.join(generatedOffersPath, file);
+      fs.unlinkSync(filePath);
+      console.log(`${filePath} was deleted`);
+    });
+  }
+
+  if (fs.existsSync(roundUpdatedPath)) {
+    fs.readdirSync(roundUpdatedPath).forEach(file => {
+      const filePath = path.join(roundUpdatedPath, file);
+      fs.unlinkSync(filePath);
+      console.log(`${filePath} was deleted`);
+    });
   }
 
   // Creating connection
